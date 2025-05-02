a76933b327a2765ae3fc31d3177fee465ba87de9
Ouzeir Impram <jhn435897@gmail.com>
2025-05-02 11:18:15 +0300

    Init

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  resources :todos do
+    resources :items
+  end
+
+  post "auth/login", to: "authentication#authenticate"
+  post "signup", to: "users#create"
+end
